d8438bb8659c1b421fd0495e2db304b824732445
z779750025 <896453263@qq.com>
2019-12-05 21:11:05 +0800

    pro modified2

--- paomao_gui.py
+++ paomao_gui.py
@@ -1,10 +1,27 @@
 from gooey import Gooey,GooeyParser
 import paomao_pro
-#from argparse import ArgumentParser
+import os
+import sys
 
-@Gooey
+
+#from argparse import ArgumentParser
+#初始化
+version="抛锚工具箱 2.2.2"
+
+WorkingDirectory= (os.path.dirname(os.path.realpath(sys.argv[0]))) #当前脚本工作路径
+print (WorkingDirectory)
+os.chdir(WorkingDirectory)#切换工作目录到脚本文件夹
+
+@Gooey(
+language='chinese',
+advanced=True,
+program_name=version,       # Defaults to script name
+program_description="抛锚",       # Defaults to ArgParse Description
+default_size=(800, 700),
+)
 def main():
-    parser = GooeyParser(description="My Cool GUI Program!") 
+    
+    parser = GooeyParser(description="工具箱模式") 
 
     parser.add_argument('data',
                             action='store',
