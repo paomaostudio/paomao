6f593d756dbdc12bcbc9559d8a526a8d74ce219f
z779750025 <896453263@qq.com>
2019-12-08 03:35:59 +0800

    modified:   paomao_pro.py 	paomao_test.py

--- paomao_pro.py
+++ paomao_pro.py
@@ -38,6 +38,13 @@ from subprocess import call,Popen
             #     print('即将转码的文件是：'+path.split(input_file)[1]+'\n')
             #     classical_mode=False
             #     time.sleep(0.1)
+def getLength(input_video):
+    #print(input_video)
+    cmd = '%s -i \"%s\" -show_entries format=duration -v quiet -of csv="p=0"' %(ffprobe,input_video)
+    output =os.popen(cmd,'r')
+    output = output.read()
+    return output
+
 slow=('标准模式',
 '(在合理的转码速度下获得不错的画质，符合bilibili不二压标准，推荐大多数情况下使用)',
 " -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset slow -X 1920 --encoder-level 4.0 -2 -T -R 44.1 --vb 5800")
@@ -49,30 +56,69 @@ slower=('超清模式',
 " -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset slower -X 1920 --encoder-level 4.0 -2 -T -R 44.1 --vb 5800")
 
 
-game='高速模式   (比标准版更高的转码速度，合理的画质)'
+game=('高速模式',
+   '(比标准版更高的转码速度，合理的画质)',
+   " -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset normal -X 1920 --encoder-level 4.0 -2 -T -R 44.1 --vb 5800")
+   
+
+wechat=('微信模式',   
+'(把视频暴力压缩到25m以下，视频时长越长，画质越差)',
+)
 
 
-wechat='微信模式   (把视频暴力压缩到25m以下，视频时长越长，画质越差)'
+demo=('演示模式',
+   '(高速，画质一般，文件小，适合给客户看的DEMO)',
+   ' -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset medium -X 960 --encoder-level 4.0 -R 44.1 --ab 32 --vb 1500'
+)
 
 
-demo="演示模式   (高速，画质一般，文件小，适合给客户看的DEMO)"
+user=('专家模式' ,  '(高级功能，如果懂得如何使用命令行版handbrake就自己设置吧！)'
+)
 
+audio=('音频提取'   '(把音轨提取出来转成320k的mp3)'
+)
 
-user="专家模式   (高级功能，如果懂得如何使用命令行版handbrake就自己设置吧！)"
-
-
-audio="音频提取   (把音轨提取出来转成320k的mp3)"
+def wechat_check(input_file):
+    print (input_file)
+    try:
+            duration=float(getLength(input_file))
+    except:
+            print(Fore.RED+'出事啦,无法计算视频长度，请手动输入视频长度，\n默认单位为秒，请直接输入数字\n'+Fore.RESET)
+            duration = input()
+    else:
+            pass
+            
+    print( '视频时长为'+str(duration)+'秒' )
     
-
+    videobitrate=20*8/duration*1024-32
+    ab=32
+    
+    if 0 < videobitrate <= 10:
+        videobitrate=20*8/duration*1024-10
+        ab=10
+    elif videobitrate < 1:
+        print("视频时长过长，无法转换，我崩溃了")
+        input()
+        exit(0)
+    elif 10<videobitrate<64:
+        print(Fore.RED+'!!!!视频时长过长，画质可能会惨不忍睹!!!!'+Fore.RESET)
+        pass
+    elif videobitrate>2000:
+        videobitrate=2000
+        pass
+    else:
+        pass
+        
+    if duration>1000:
+        print ("警告：视频时长较长，画质可能会惨不忍睹")
+    return (" -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset slow -X 640 --encoder-level 4.0 -2 -T --ab %s -R 44.1 --vb "%(ab)+str(videobitrate))       
+            
 def main():
         
     pygame.mixer.init()
-    colorama.init(autoreset=True)
-    #git推送
-    #tprint('PAOMAO',font='slant')
-
-    #print(argv[0])          #argv[0] 类似于shell中的$0,但不是脚本名称，而是脚本的路径   
-    #rint(argv[1])          #argv[1] 表示传入的第一个参数，既 hell
+    colorama.init(autoreset=True) 
+    #argv[0] 脚本的路径
+    #argv[1] 表示传入的第一个参数
 
     file_split=path.splitext(input_file)    #文件名和后缀分割开
 
@@ -80,12 +126,7 @@ def main():
         prompt='\n当前转码预设：'+Fore.YELLOW+preset_name+Fore.RESET+' \n即将开始转码~\n'
         print(prompt)
 
-    def getLength(input_video):
-        #print(input_video)
-        cmd = '%s -i \"%s\" -show_entries format=duration -v quiet -of csv="p=0"' %(ffprobe,input_video)
-        output =os.popen(cmd,'r')
-        output = output.read()
-        return output
+    
 
 
     output_file=file_split[0]+"_pm"+".mp4"
@@ -117,55 +158,12 @@ def main():
         pass
     elif preset == slower:
         
-        encode_arg=
+        #encode_arg=
         show_prompt('超清版')
         paomao(encode_arg)
 
     elif preset == wechat:
-        try:
-            duration=float(getLength(input_file))
-        except:
-            print(Fore.RED+'出事啦,无法计算视频长度，请手动输入视频长度，\n默认单位为秒，请直接输入数字\n'+Fore.RESET)
-
-            duration = input()
-
-        else:
-            pass
-
-
-        print( '视频时长为'+str(duration)+'秒' )
-        videobitrate=20*8/duration*1024-32
-        ab=32
-        #print(videobitrate)
-        if 0 < videobitrate <= 10:
-            videobitrate=20*8/duration*1024-10
-            ab=10
-            if videobitrate < 1:
-
-                print("视频时长过长，无法转换，我崩溃了")
-                input()
-                exit(0)
-            else:
-                pass
-
-        if 10<videobitrate<64:
-            print(Fore.RED+'!!!!视频时长过长，画质可能会惨不忍睹!!!!'+Fore.RESET)
-            pass
-        if videobitrate>2000:
-            videobitrate=2000
-            pass
-        if duration>1000:
-            wechat_choices = ["我不管，我就要！", "害怕！那算了~"]
-            warning = ui.ask_choice(
-                "温馨提示:\n---------------------------------------------------------------------------------------\n视频时长超过10分钟！\
-                    微信转码的规则是无论视频多长，都要压缩到25m以内，\n视频文件体积=(音频码率+视频码率) x 时长 / 8\n视频越长画质越差，10分钟以上的视频画质通常惨不忍睹\n确定要继续吗？\
-                    \n---------------------------------------------------------------------------------------", choices=wechat_choices
-                    )
-            if warning == "我不管，我就要！":
-                pass
-            else:
-                
-                exit(0)
+        
         encode_arg=" -e x264 --encoder-tune psnr --crop 0:0:0:0 --encoder-preset slow -X 640 --encoder-level 4.0 -2 -T --ab %s -R 44.1 --vb "%(ab)+str(videobitrate)
         output_file=file_split[0]+"_wechat"+".mp4"
         show_prompt('微信版')
@@ -199,6 +197,7 @@ def main():
         print ('如果你看到这条消息 说明程序出错了 可以考虑联系飘渺酱')
         exit(0)
         pass
+
 
     if os.path.exists(output_file):
         music=pygame.mixer.Sound('Congrats.wav')
